90a43dab619e871749cb093b5dce96419d8789de
cubasve <eva.cubasvasquez@gmail.com>
2020-10-03 23:11:24 -0400

    Add skill icons and project descriptions

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
-import { faDonate, faTshirt, faGlobeAmericas, faWalking } from '@fortawesome/free-solid-svg-icons';
+import { AppBar, Tabs, Tab, Typography, Box, Button } from '@material-ui/core';
+import { faDonate, faTshirt, faGlobeAmericas, faStreetView } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -40,6 +40,23 @@ const useStyles = makeStyles(theme => ({
     root: { backgroundColor: theme.palette.background.paper, width: 500 }
 }))
 
+const html = <i class="devicon-html5-plain colored"></i>;
+const css = <i class="devicon-css3-plain colored"></i>;
+// const bootstrap = <i class="devicon-bootstrap-plain colored"></i>;
+const javascript = <i class="devicon-javascript-plain colored"></i>;
+const react = <i class="devicon-react-original colored"></i>
+const jQuery = <i class="devicon-jquery-plain colored"></i>;
+const postgresql = <i class="devicon-postgresql-plain colored"></i>;
+const python = <i class="devicon-python-plain colored"></i>;
+const django = <i class="devicon-django-plain"></i>;
+const express = <i class="devicon-express-original"></i>;
+const node = <i class="devicon-nodejs-plain colored"></i>;
+const mongoDB = <i class="devicon-mongodb-plain colored"></i>;
+const git = <i class="devicon-git-plain colored"></i>;
+const gitHub = <i class="devicon-github-plain colored"></i>;
+const heroku = <i class="devicon-heroku-plain colored"></i>;
+
+
 export default function Projects() {
 
     const classes = useStyles();
@@ -57,7 +74,7 @@ export default function Projects() {
     const finesse = <FontAwesomeIcon icon={faDonate} size="3x" />
     const swapify = <FontAwesomeIcon icon={faTshirt} size="3x" />
     const slashTrash = <FontAwesomeIcon icon={faGlobeAmericas} size="3x" />
-    const hangman = <FontAwesomeIcon icon={faWalking} size="3x" />
+    const hangman = <FontAwesomeIcon icon={faStreetView} size="3x" />
 
     return (
         <>
@@ -84,9 +101,39 @@ export default function Projects() {
                 index={value}
                 onChangeIndex={handleChangeIndex}
             >
-                <TabPanel value={value} index={0} dir={theme.direction}>About Finesse: Description, Technologies, Screenshot/Gif</TabPanel>
-                <TabPanel value={value} index={1} dir={theme.direction}>About Swapify <i class="fas fa-donate"></i></TabPanel>
-                <TabPanel value={value} index={2} dir={theme.direction}>About Slash Trash</TabPanel>
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <div><img src="./Finesse.png"></img></div>
+                    <div>
+                        <h1>Finesse</h1>
+                        <h2>Financial Statement Tracker</h2>
+                        <h2>Technologies: {mongoDB} {express} {react} {node}</h2>
+                        Finesse tracks a user's personal financial statements and teaches financial literacy. It aims to demystify financial jargon so it is easy to understand. This application's concepts are based on Robert Kiyosaki's "Increase Your Financial IQ," with additional features implemented.
+                        <Button variant="contained" color="primary" href="https://drippininfinesse.herokuapp.com/" target="_blank">Demo</Button>
+                        <Button variant="contained" color="primary" href="https://github.com/cubasve/Finesse" target="_blank">Code</Button>
+                    </div>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    <div><img src="./Swapify.png"></img></div>
+                    <div>
+                        <h1>Swapify</h1>
+                        <h2>Second-Hand Clothing and Shoe Exchange</h2>
+                        <h2>Technologies: {postgresql} {python} <h1>{django}</h1></h2>
+                        Swapify is a collaborative project built by 5 developers. Shoes and clothing posted by the same user can be recommended to be styled together to form an outfit. Its purpose is to prevent fast fashion from being sent to landfills and instead promote the second-hand market.
+                        <Button variant="contained" color="primary" href="https://teamswapify.herokuapp.com/" target="_blank">Demo</Button>
+                        <Button variant="contained" color="primary" href="https://github.com/cubasve/Swapify" target="_blank">Code</Button>
+                    </div>
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    <div>Enter image</div>
+                    <div>
+                        <h1>Slash Trash</h1>
+                        <h2>Eco-Friendly Household Product Tracker</h2>
+                        <h2>Technologies: {mongoDB} {express} {node}</h2>
+                        Slash Trash is a platform for users to search for eco-friendly alternatives to everyday household items and start their journey to a low-waste lifestyle. This application provides actionable steps to go green by allowing the user the ability to keep track of the changes they implemented.
+                        <Button variant="contained" color="primary" href="https://slash-trash.herokuapp.com/" target="_blank">Demo</Button>
+                        <Button variant="contained" color="primary" href="https://github.com/cubasve/Slash-Trash" target="_blank">Code</Button>
+                    </div>
+                </TabPanel>
                 <TabPanel value={value} index={3} dir={theme.direction}>About Hangman</TabPanel>
             </SwipeableViews>
             {/* </div> */}
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3Alt, faJsSquare, faReact, faNode, faPython, faGitAlt, faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { faHtml5, faCss3Alt, faJsSquare, faReact, faNode, faPython, faGitAlt, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const useStyles = makeStyles({
     root: {
@@ -17,45 +17,54 @@ const useStyles = makeStyles({
     },
 });
 
+const html = <i class="devicon-html5-plain colored"></i>;
+const css = <i class="devicon-css3-plain colored"></i>;
+// const bootstrap = <i class="devicon-bootstrap-plain colored"></i>;
+const javascript = <i class="devicon-javascript-plain colored"></i>;
+const react = <i class="devicon-react-original colored"></i>
+const jQuery = <i class="devicon-jquery-plain colored"></i>;
+const postgresql = <i class="devicon-postgresql-plain colored"></i>;
+const trello = <i class="devicon-trello-plain colored"></i>;
+const vsCode = <i class="devicon-visualstudio-plain colored"></i>;
+const python = <i class="devicon-python-plain colored"></i>;
+const django = <i class="devicon-django-plain colored"></i>;
+const express = <i class="devicon-express-original colored"></i>;
+const node = <i class="devicon-nodejs-plain colored"></i>;
+const mongoDB = <i class="devicon-mongodb-plain colored"></i>;
+const git = <i class="devicon-git-plain colored"></i>;
+const gitHub = <i class="devicon-github-plain colored"></i>;
+const heroku = <i class="devicon-heroku-plain colored"></i>;
+
 export default function Skills() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>Bullet</span>
 
     return (
         <>
             <h1>Skills</h1>
             <Card className={classes.root}>
                 <CardContent>
-                    <Typography variant="h5" component="h2">Front-End</Typography>
-                    <Typography variant="body2" component="p">
-                        <FontAwesomeIcon icon={faHtml5} size="3x" />
-                        <FontAwesomeIcon icon={faCss3Alt} size="3x" />
-                        <FontAwesomeIcon icon={faJsSquare} size="3x" />
-                        <FontAwesomeIcon icon={faReact} size="3x" />
-                        <i className="devicon-css3-plain  icon"></i>
-                        <p>Add jQuery</p>
+                    <Typography variant="h4" component="h2">Front-End</Typography>
+                    <Typography variant="h3" component="h1">
+                        {/* <FontAwesomeIcon icon={faHtml5} size="3x" /> */}
+                        {html} {css} {javascript} {react} {jQuery}
                     </Typography>
                 </CardContent>
             </Card>
 
             <Card className={classes.root}>
                 <CardContent>
-                    <Typography variant="h5" component="h2">Back-End</Typography>
-                    <Typography variant="body2" component="p">
-                        <FontAwesomeIcon icon={faNode} size="3x" />
-                        <FontAwesomeIcon icon={faPython} size="3x" />
-                        <p>Add Express.js, MongoDB, Django, PostgreSQL</p>
+                    <Typography variant="h4" component="h2">Back-End</Typography>
+                    <Typography variant="h3" component="p">
+                        {express} {node} {python} {django} {mongoDB} {postgresql}
                     </Typography>
                 </CardContent>
             </Card>
 
             <Card className={classes.root}>
                 <CardContent>
-                    <Typography variant="h5" component="h2">Deployment</Typography>
-                    <Typography variant="body2" component="p">
-                        <FontAwesomeIcon icon={faGitAlt} size="3x" />
-                        <FontAwesomeIcon icon={faGithub} size="3x" />
-                        <p>Add Heroku, MongoDBAtlas, Command Line</p>
+                    <Typography variant="h4" component="h2">Tools</Typography>
+                    <Typography variant="h3" component="p">
+                        {git} {gitHub} {heroku} {trello} {vsCode}
                     </Typography>
                 </CardContent>
             </Card>
