567b536664c3e81ae7d731f60b98bd41bb9445f5
cubasve <eva.cubasvasquez@gmail.com>
2021-01-23 12:38:35 -0500

    Update readme screenshots, finish styling About.jsx and add html and css tech to projects

--- README.md
+++ README.md
@@ -11,15 +11,15 @@
 My portfolio website that showcases my projects.
 
 ## Screenshots
-![image](https://user-images.githubusercontent.com/62129720/95937700-595e0c00-0da6-11eb-87e0-da834af64ad1.png)
+![image](https://user-images.githubusercontent.com/62129720/105609236-f5b88700-5d75-11eb-80cb-c8f995f10e17.png)
 
-![image](https://user-images.githubusercontent.com/62129720/95937763-7b578e80-0da6-11eb-9580-a860dfe47b1f.png)
+![image](https://user-images.githubusercontent.com/62129720/105609254-0b2db100-5d76-11eb-986a-b42131aea0f2.png)
 
-![image](https://user-images.githubusercontent.com/62129720/95937809-988c5d00-0da6-11eb-862c-561db29bfeeb.png)
+![image](https://user-images.githubusercontent.com/62129720/105609261-1bde2700-5d76-11eb-89fb-dab62d203782.png)
 
-![image](https://user-images.githubusercontent.com/62129720/95937861-b22da480-0da6-11eb-8c32-68797d2ca80a.png)
+![image](https://user-images.githubusercontent.com/62129720/105609277-36180500-5d76-11eb-8836-7945d16eabc5.png)
 
-![image](https://user-images.githubusercontent.com/62129720/95937919-cbceec00-0da6-11eb-973d-1a8bde9e3094.png)
+![image](https://user-images.githubusercontent.com/62129720/105609303-5ba50e80-5d76-11eb-83cb-e6c025115904.png)
 
 ## Technologies
 - React
@@ -37,4 +37,3 @@ My portfolio website that showcases my projects.
 
 ## Next Steps
 - Navbar: position fixed or sticky
-- Update introduction
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './About.css';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, IconButton } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
-// import OfflineBoltIcon from '@material-ui/icons/OfflineBolt';
 import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 
-const useStyles = makeStyles({
-    icon: {
-    },
-});
 
 export default function About() {
-    const classes = useStyles();
 
     return (
         <>
@@ -24,12 +17,12 @@ export default function About() {
                 </div>
 
                 <div className="brand">
-                    <Typography>
-                        <IconButton><AccountTreeIcon color="primary" className={classes.icon} style={{ fontSize: 40 }} /></IconButton><span className="statement development">I am passionate about web development and personal development, which are merged together to create my projects.</span>
-                    </Typography>
-                    <Typography>
-                        <IconButton><WbIncandescentIcon color="primary" className={classes.icon} style={{ fontSize: 40 }} /></IconButton><span className="statement">My mission is to make a positive impact in a technology-driven world by using innovation to engineer software.</span>
-                    </Typography>
+                    <div className="one statement">
+                        <IconButton><AccountTreeIcon color="primary" style={{ fontSize: 40 }} /></IconButton><span className="statement development">I am passionate about web development and personal development, which are merged together to create my projects.</span>
+                    </div>
+                    <div className="two statement">
+                        <IconButton><WbIncandescentIcon color="primary" style={{ fontSize: 40 }} /></IconButton><span className="statement">My mission is to make a positive impact in a technology-driven world by using innovation to engineer software.</span>
+                    </div>
                 </div>
             </div>
         </>
