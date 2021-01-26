ce69bd2b8a8531cc040c954fc40e2e8d3199b3bb
cubasve <eva.cubasvasquez@gmail.com>
2021-01-25 22:21:53 -0500

    Change navbar back to words - users may not understand icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { AppBar, Link, Tooltip, Zoom } from '@material-ui/core';
+import { AppBar, Link } from '@material-ui/core';
 import Scrollspy from 'react-scrollspy';
-import HomeIcon from '@material-ui/icons/Home';
-import PersonIcon from '@material-ui/icons/Person';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import CodeIcon from '@material-ui/icons/Code';
-import PhoneIcon from '@material-ui/icons/Phone';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHome, faUser, faBriefcase, faLaptopCode, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles({
     link: {
         color: 'white',
+        fontSize: '19px',
         margin: 15,
-        textAlign: 'center',
+        // textAlign: 'center',
     },
 });
-
-const LightTooltip = withStyles((theme) => ({
-    tooltip: {
-        backgroundColor: 'tan',
-        color: 'black',
-        boxShadow: theme.shadows[1],
-        fontSize: 16,
-    },
-}))(Tooltip);
 
 export default function Navbar() {
     const classes = useStyles();
@@ -31,23 +20,19 @@ export default function Navbar() {
         <>
             <AppBar position="fixed">
                 <Scrollspy items={['intro', 'about', 'projects', 'skills', 'contact']} currentClassName="is-current" offset={0}>
-                    {/* <div id="navbar-icons"> */}
-                    <LightTooltip title="Home" TransitionComponent={Zoom}>
-                        <Link href="#intro" className={classes.link} id="nav-links"><HomeIcon fontSize="large" /></Link>
-                    </LightTooltip>
-                    <LightTooltip title="About" TransitionComponent={Zoom}>
-                        <Link href="#about" className={classes.link} id="nav-links"><PersonIcon fontSize="large" /></Link>
-                    </LightTooltip>
-                    <LightTooltip title="Projects" TransitionComponent={Zoom}>
-                        <Link href="#projects" className={classes.link} id="nav-links"><AssignmentIcon fontSize="large" /></Link>
-                    </LightTooltip>
-                    <LightTooltip title="Skills" TransitionComponent={Zoom}>
-                        <Link href="#skills" className={classes.link} id="nav-links"><CodeIcon fontSize="large" /></Link>
-                    </LightTooltip>
-                    <LightTooltip title="Contact" TransitionComponent={Zoom}>
-                        <Link href="#contact" className={classes.link} id="nav-links"><PhoneIcon fontSize="large" /></Link>
-                    </LightTooltip>
-                    {/* </div> */}
+                    <Link href="#intro" className={classes.link} id="nav-links">
+                        {/* <FontAwesomeIcon icon={faHome} /> */}
+                         Home</Link>
+                    <Link href="#about" className={classes.link}>
+                        {/* <FontAwesomeIcon icon={faUser} /> */}
+                         About</Link>
+                    <Link href="#projects" className={classes.link}>
+                        {/* <FontAwesomeIcon icon={faBriefcase} /> */}
+                         Projects</Link>
+                    <Link href="#skills" className={classes.link}>
+                        {/* <FontAwesomeIcon icon={faLaptopCode} /> */}
+                         Skills</Link>
+                    {/* <Link href="#contact" className={classes.link}><FontAwesomeIcon icon={faPhoneAlt} /> Contact</Link> */}
                 </Scrollspy>
             </AppBar>
         </>
